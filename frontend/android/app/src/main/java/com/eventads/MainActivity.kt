369ebc41dc8b71eb0e4999bf8bc8a70058722feb
Danilo Humberto <danilohumberto12@hotmail.com>
2026-06-15 13:44:04 -0300

    feat: update project name and package identifiers to EventADS

--- frontend/android/app/src/main/java/com/eventads/MainActivity.kt
+++ frontend/android/app/src/main/java/com/eventads/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.frontend
+package com.eventads
 
 import android.os.Build
 import android.os.Bundle
--- frontend/android/app/src/main/java/com/eventads/MainApplication.kt
+++ frontend/android/app/src/main/java/com/eventads/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.frontend
+package com.eventads
 
 import android.app.Application
 import android.content.res.Configuration
